da7eacd123e9a90028f7458753a1b62c06f522d5
Voodoo51 <vooodooo51@gmail.com>
2024-10-20 10:27:15 +0200

    Pierwszy Commit.

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+import static com.raylib.Raylib.*;
+import static com.raylib.Jaylib.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        InitWindow(800, 450, "Demo");
+        SetTargetFPS(60);
+        Camera2D camera = new Camera2D();
+        Texture tex = LoadTexture("Textures/awesomeface.png");
+        tex.width(20);
+        tex.height(20);
+        while (!WindowShouldClose()) {
+            BeginDrawing();
+            DrawTexture(tex, 100, 100, WHITE);
+            ClearBackground(RAYWHITE);
+            BeginMode2D(camera);
+            EndMode2D();
+            DrawText("Hello world", 190, 200, 20, VIOLET);
+            DrawFPS(20, 20);
+            EndDrawing();
+        }
+        CloseWindow();
+    }
+}
